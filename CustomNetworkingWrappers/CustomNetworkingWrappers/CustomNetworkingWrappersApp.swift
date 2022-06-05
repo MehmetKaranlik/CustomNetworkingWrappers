452134a4567e282833341334854ba8a4cefaa74a
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-06-05 14:08:59 +0300

    Feat : Initiliaze

---
+++ CustomNetworkingWrappers/CustomNetworkingWrappers/CustomNetworkingWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNetworkingWrappersApp.swift
+//  CustomNetworkingWrappers
+//
+//  Created by mehmet karanlık on 5.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomNetworkingWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
